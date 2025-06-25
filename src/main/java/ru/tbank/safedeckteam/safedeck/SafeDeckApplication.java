01ff21f38279957ba5c7115e4121c39e092283d5
vlint <lintvadim@gmail.com>
2025-06-25 20:22:04 +0300

    Init commit

---
+++ src/main/java/ru/tbank/safedeckteam/safedeck/SafeDeckApplication.java
@@ -0,0 +1,13 @@
+package ru.tbank.safedeckteam.safedeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeDeckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeDeckApplication.class, args);
+    }
+
+}
