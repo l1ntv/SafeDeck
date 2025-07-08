8b71b890574fe47a3b2684c405486dc3700be8f4
vlint <lintvadim@gmail.com>
2025-07-08 10:32:02 +0300

    Профиль связан с основной веткой

---
+++ src/main/java/ru/tbank/safedeckteam/safedeck/service/impl/ProfileServiceImpl.java
@@ -0,0 +1,34 @@
+package ru.tbank.safedeckteam.safedeck.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.tbank.safedeckteam.safedeck.model.Client;
+import ru.tbank.safedeckteam.safedeck.repository.ClientRepository;
+import ru.tbank.safedeckteam.safedeck.service.ProfileService;
+import ru.tbank.safedeckteam.safedeck.web.dto.PublicNameResponseDTO;
+import ru.tbank.safedeckteam.safedeck.web.dto.UpdatePublicNameRequestDTO;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileServiceImpl implements ProfileService {
+
+    private final ClientRepository clientRepository;
+
+    @Override
+    public PublicNameResponseDTO getCurrentUserPublicName(String email) {
+        Client client = clientRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Client not found."));
+        return new PublicNameResponseDTO(client.getPublicName());
+    }
+
+    @Override
+    @Transactional
+    public PublicNameResponseDTO updatePublicName(String email, UpdatePublicNameRequestDTO requestDto) {
+        Client client = clientRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Client not found."));
+        client.setPublicName(requestDto.getNewPublicName());
+        clientRepository.save(client);
+        return new PublicNameResponseDTO(requestDto.getNewPublicName());
+    }
+}
---
+++ src/main/java/ru/tbank/safedeckteam/safedeck/web/dto/PublicNameResponseDTO.java
@@ -0,0 +1,13 @@
+package ru.tbank.safedeckteam.safedeck.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PublicNameResponseDTO {
+
+    private String publicName;
+}
