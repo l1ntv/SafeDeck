24c062a8098d79721c1bd19238a1449b259a5eb4
vlint <lintvadim@gmail.com>
2025-07-19 23:17:49 +0300

    Исправил docker-compose

--- safedeck-email-service/src/main/java/ru/tbank/safedeckteam/safedeckemailservice/SafedeckEmailServiceApplication.java
+++ safedeck-email-service/src/main/java/ru/tbank/safedeckteam/safedeckemailservice/SafedeckEmailServiceApplication.java
@@ -2,8 +2,9 @@ package ru.tbank.safedeckteam.safedeckemailservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SafedeckEmailServiceApplication {
 
     public static void main(String[] args) {
